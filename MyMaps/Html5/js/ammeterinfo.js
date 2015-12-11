d7324e01635381eda3a5d4c8a0676dda27b886b5
Vktun <Vktun@users.noreply.github.com>
2015-12-11 17:49:12 +0800

    Initial commit

---
+++ MyMaps/Html5/js/ammeterinfo.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Administrator on 2016/3/28.
+ */
+var ammeterinfo = [
+    {
+        title: "电表1",
+        id: 1,
+        x: 20,
+        y: 23,
+        status: 1
+    },
+    {
+        title: "电表2",
+        id: 2,
+        x: 135,
+        y: 137,
+        status: 1
+    },
+    {
+        title: "电表3",
+        id: 3,
+        x: 270,
+        y: 85,
+        status: 1
+    },
+    {
+        title: "电表4",
+        id: 4,
+        x: 240,
+        y: 245,
+        status: 0
+    }, {
+        title: "电表5",
+        id: 5,
+        x: 340,
+        y: 145,
+        status: 0
+    }
+]
---
+++ MyMaps/Html5/js/ammeters.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Administrator on 2016/3/30.
+ */
+var ammeters=[
+    {
+        title: "电表1",
+        id: 1,
+        status: 1
+    },
+    {
+        title: "电表2",
+        id: 2,
+        status: 1
+    },
+    {
+        title: "电表3",
+        id: 3,
+        status: 1
+    },
+    {
+        title: "电表4",
+        id: 4,
+        status: 0
+    }, {
+        title: "电表5",
+        id: 5,
+        status: 0
+    }
+    ,{
+        title: "电表6",
+        id: 6,
+        status: 1
+    },
+    {
+        title: "电表6",
+        id: 6,
+        status: 1
+    },
+    {
+        title: "电表7",
+        id: 7,
+        status: 1
+    },
+    {
+        title: "电表8",
+        id: 8,
+        status: 0
+    }, {
+        title: "电表9",
+        id: 9,
+        status: 0
+    }
+];
+
